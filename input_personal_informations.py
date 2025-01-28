d4ff3286b3365b09052d2dabbfde1ce3fd0d5d90
izzokewl <gozezekiel06@gmail.com>
2025-01-28 14:10:00 +0800

    name error established

--- input_personal_informations.py
+++ input_personal_informations.py
@@ -1,9 +1,9 @@
-nameinput = input("Enter your full name: ").title()
-print ("Name: ")
-print (nameinput)
-ageinput = int(input("Enter your age: "))
-print ("Age: ")
-print (ageinput)
-addressinput = input("Enter your address: ").title()
-print ("Address: ")
-print (addressinput)
+while True:
+    try:
+        nameinput = input("Enter your full name: ").title()
+        if any(char.isdigit() for char in nameinput):
+            raise ValueError("HOI BAT MAY NUMBER PANGALAN MO! ULET")
+        print(f"Name: {nameinput}")
+        break
+    except ValueError as error:
+        print (error)
