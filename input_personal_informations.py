a2a5b71912083f6145ab9eb3634cd04e347a198b
izzokewl <gozezekiel06@gmail.com>
2025-01-28 14:39:13 +0800

    added input questions

--- input_personal_informations.py
+++ input_personal_informations.py
@@ -3,16 +3,24 @@ while True:
         nameinput = input("Enter your full name: ").title()
         if any(char.isdigit() for char in nameinput):
             raise ValueError("HOI BAT MAY NUMBER PANGALAN MO! ULET")
-        print(f"Name: {nameinput}")
+        print(f"Ikaw si {nameinput}")
         break
     except ValueError as error:
         print (error)
+
 while True:
     try:
         ageinput = int(input("Enter your age: "))
-        print (f"Age: {ageinput}")
+        print (f"Ang edad mo ay {ageinput}")
         break
     except ValueError:
-        print ("gets kita, pero pwede bang gawin mong numbers yan")
+        print ("gets kita, pero pwede bang gawin mong numbers yan...")
+
 addressinput = input("Enter your address: ").title()
-print (f"Address: {addressinput}")
+print (f"Jan ka pala nakatira haha, sa {addressinput}")
+
+emailinput = input("Enter your Email address: ")
+print (f"Hack ka sakin dejk ang email mo ay {emailinput}")
+
+feelingrn = input ("What are you feeling as of the moment?")
+print (f"ok gets kita sana malagpasan mo yan")
